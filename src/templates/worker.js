b13094c47c31f99f1ff421c3b827e9b48fe5abaa
Skillz <gankstarsleague@gmail.com>
2019-08-09 13:16:59 -0400

    finalizing worker page

--- src/templates/worker.js
+++ src/templates/worker.js
@@ -5,6 +5,23 @@ import blueprints from "../constants/blueprints"
 import Blueprint from "../components/Blueprint"
 import Worker from "../components/Worker"
 import SEO from "../components/seo"
+import { cleanName } from "../utils/util"
+import { withStyles } from "@material-ui/core/styles"
+import AppBar from "@material-ui/core/AppBar"
+import Tabs from "@material-ui/core/Tabs"
+import Tab from "@material-ui/core/Tab"
+import Typography from "@material-ui/core/Typography"
+import PropTypes from "prop-types"
+
+const HeroImg = styled.img`
+  position: relative;
+  margin-left: 25px;
+  height: 200px;
+  width: 200px;
+  display: block;
+  padding-bottom: 10px;
+  padding-top: 10px;
+`
 
 const Title = styled.h1`
   font-family: Roboto;
@@ -44,28 +61,133 @@ const Container = styled.div`
   position: relative;
 `
 
-export default ({ pageContext: data }) => {
-  const relevantBlueprints = blueprints.filter(
-    b =>
-      b["Required Worker"] === data.title ||
-      b["Required Worker__1"] === data.title
-  )
+const WorkerCard = styled.div`
+  display: flex;
+`
 
+const StyledAppBar = withStyles({
+  root: {
+    background: "none",
+    width: "max-content",
+    margin: "0 auto",
+    boxShadow: "none",
+  },
+})(AppBar)
+
+const StyledTabs = withStyles({
+  indicator: {
+    display: "none",
+    backgroundColor: "none",
+    color: "none",
+  },
+})(Tabs)
+
+const StyledTab = withStyles({
+  root: {
+    color: "grey",
+  },
+  selected: {
+    background: "#5FA9FF",
+    borderRadius: "10px",
+    marginRight: "16px",
+  },
+  label: {
+    color: "white",
+  },
+})(Tab)
+
+function TabContainer({ children, dir }) {
   return (
-    <Layout>
-      <SEO title={data.name}></SEO>
-      <Section>
-        <Container />
-      </Section>
-      <Worker details={data} type="worker" />
-      <Title>Blueprints</Title>
-      <Resources>
-        {relevantBlueprints.map((blueprint, index) => (
-          <div key={index}>
-            <Blueprint details={blueprint}></Blueprint>
-          </div>
-        ))}
-      </Resources>
-    </Layout>
+    <Typography
+      component="div"
+      dir={dir}
+      style={{ padding: 8 * 3, background: "#F4FAFF" }}
+    >
+      {children}
+    </Typography>
   )
 }
+
+TabContainer.propTypes = {
+  children: PropTypes.node.isRequired,
+  dir: PropTypes.string.isRequired,
+}
+
+const styles = theme => ({
+  root: {
+    backgroundColor: theme.palette.background.paper,
+    width: 500,
+  },
+})
+
+class FullWidthTabs extends React.Component {
+  state = {
+    value: 0,
+  }
+
+  handleChange = (event, value) => {
+    this.setState({ value, heroValue: value === 1 ? 0 : null })
+  }
+
+  handleChangeIndex = index => {
+    this.setState({ value: index })
+  }
+
+  render() {
+    const data = this.props.pageContext
+    const { value } = this.state
+
+    const relevantBlueprints = blueprints.filter(
+      b =>
+        b["Required Worker"] === data.title ||
+        b["Required Worker__1"] === data.title
+    )
+
+    return (
+      <Layout>
+        <SEO title={data.name}></SEO>
+        <Section>
+          <Container />
+        </Section>
+        <div className="CardboxGroupScroll" style={{ marginBottom: "50px" }}>
+          <Resources className="CardboxGroup">
+            <Worker details={data} type="worker" />
+          </Resources>
+        </div>
+
+        <StyledAppBar position="static" color="default">
+          <StyledTabs
+            value={this.state.value}
+            onChange={this.handleChange}
+            indicatorColor="primary"
+            textColor="primary"
+            variant="fullWidth"
+          >
+            <StyledTab label="BLUEPRINTS" className="button" />
+            <StyledTab label="LEVELS" className="button" />
+          </StyledTabs>
+        </StyledAppBar>
+        {value === 0 && (
+          <TabContainer>
+            <div className="CardboxGroupScroll">
+              <Resources className="CardboxGroup">
+                {relevantBlueprints.map((blueprint, index) => (
+                  <div key={index}>
+                    <Blueprint details={blueprint}></Blueprint>
+                  </div>
+                ))}
+              </Resources>
+            </div>
+          </TabContainer>
+        )}
+      </Layout>
+    )
+  }
+}
+
+FullWidthTabs.propTypes = {
+  classes: PropTypes.object.isRequired,
+  theme: PropTypes.object.isRequired,
+}
+
+export default withStyles(styles, { withTheme: true })(FullWidthTabs)
