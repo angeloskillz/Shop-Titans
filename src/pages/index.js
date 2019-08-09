1916d3847e0a898d6936d0c3dd2f45180f7dc674
Skillz <gankstarsleague@gmail.com>
2019-08-09 14:13:08 -0400

    adding discord links to header

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import ImgHero from "gatsby-image"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Homepage from "../components/Homepage"
+import { Button } from "@material-ui/core"
 
 const IndexPage = props => (
   <Layout>
@@ -20,7 +21,17 @@ const IndexPage = props => (
       </div>
       <div className="HeroGroup">
         <h1>Shop Titans</h1>
-        <p>Update 2.0.1</p>
+        <p style={{ marginBottom: "15px" }}>Update 2.0.1</p>
+        <a href="https://discord.gg/shoptitans" style={{ margin: "15px" }}>
+          <Button variant="contained" color="primary">
+            Offical Discord
+          </Button>
+        </a>
+        <a href="https://discord.gg/rWMuMdk">
+          <Button variant="contained" color="secondary">
+            Wiki Discord
+          </Button>
+        </a>
       </div>
     </div>
     <Homepage />
