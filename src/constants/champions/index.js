035e00a4d2bfaa9741bf80a7269eba865e070329
Skillz <gankstarsleague@gmail.com>
2019-09-16 18:05:57 -0400

    add yami and lilu lore

--- src/constants/champions/index.js
+++ src/constants/champions/index.js
@@ -269,16 +269,113 @@ module.exports = [
             effect: "Heals party by 3 HP each turn. 10% faster resting time.",
           },
         },
+        title: "Precocious Apprentice",
+        story: [
+          {
+            name: "Lilu",
+            text:
+              "Well, Pollux? What do you think of this new spell? Perfect, right? Yeah, this time we'll ace it for sure!",
+          },
+          {
+            name: "Maribel",
+            text:
+              "Good morning, Lilu. Here for the weekly shipment of Living Roots?",
+          },
+          { name: "Lilu", text: "Yes, thank you, Maribel!" },
+          {
+            name: "Maribel",
+            text:
+              "You sure ordered a lot this week! What's your master doing with all of these?",
+          },
+          {
+            name: "Lilu",
+            text:
+              "To be honest, about half of it is for me. I'm doing some serious studying!",
+          },
+          { name: "Maribel", text: "Oh? What kind?" },
+          {
+            name: "Lilu",
+            text:
+              "Healing spells! I've spent all of last week mending cuts and bruises at the Training Hall!",
+          },
+          {
+            name: "Maribel",
+            text:
+              "Right, I'm sure the Heroes are glad to have you around. ...And Argon, too!",
+          },
+          { name: "Lilu", text: "...! I- I gotta go! Thanks!" },
+        ],
       },
       {
         rank: 2,
         coins: 15,
         reward: { health: 2, attack: 74, defense: 19, skill: "---" },
+        title: "Grim Tidings",
+        story: [
+          {
+            name: "Lilu",
+            text: "Here you go, Master Grimar. This week's Living Roots.",
+          },
+          {
+            name: "Grimar",
+            text: "Thank you, Lola. Here is today's homework.",
+          },
+          {
+            name: "Lilu",
+            text:
+              "Oh! A magic tome! Is it about healing!? 'The Miscreant's Manual to Maledictions'...? This book is way too advanced! It's even got an entire section about curses!",
+          },
+          { name: "Grimar", text: "...Curses? Hmm... curses...?" },
+          { name: "Lilu", text: "Master?" },
+          { name: "Grimar", text: "35 years ago, on this very day... I...!" },
+          { name: "Lilu", text: "Master Grimar!?" },
+          { name: "Grimar", text: "...I forgot. And who might you be?" },
+          {
+            name: "Lilu",
+            text:
+              "...I'm Lilu, your apprentice. Or Lola, sometimes... Oh well, nevermind. I'll be studying this book. Thank you, Grim.",
+          },
+        ],
       },
       {
         rank: 3,
         coins: 25,
         reward: { health: 6, attack: 223, defense: 58, skill: "---" },
+        title: "Addled Mind",
+        story: [
+          {
+            name: "Lilu",
+            text:
+              "... Add in a pinch of Silver Dust and... done! Look at it glow! Not bad, huh Pollux? If the Miscreant's Manual's to Malediction is correct, this potion should-",
+          },
+          {
+            name: "Grimar",
+            text: "Lola, what is all this ruckus? I'm trying to sleep here!",
+          },
+          {
+            name: "Lilu",
+            text:
+              "Master Grimar! Thanks to your book, I brewed a very advanced potion! It should-",
+          },
+          {
+            name: "Grimar",
+            text:
+              "You did what!? Well then, give it here! Let's have a taste! ...",
+          },
+          { name: "Lilu", text: "Master Grimar?" },
+          {
+            name: "Grimar",
+            text:
+              "Lilu! I-mu-mu-must be fr-freed! Le-let me out of this doltish, half-witted mm-mind!",
+          },
+          { name: "Lilu", text: "M-master Grimar! Are you well?" },
+          {
+            name: "Grimar",
+            text:
+              "... *Burp* A little fruity! Not bad, Lola. Now then, back to your chores.",
+          },
+          { name: "Lilu", text: "... That was NOT normal." },
+        ],
       },
       {
         rank: 4,
@@ -292,6 +389,58 @@ module.exports = [
             effect: "Heals party by 5 HP each turn. 20% faster resting time.",
           },
         },
+        title: "Bloodline's End",
+        story: [
+          {
+            name: "Lilu",
+            text:
+              "Something isn't right with Master Grimar... His voice, it was as though someone was speaking from within him! Maybe we're dealing with a complex transmutation spell? Or maybe even-",
+          },
+          {
+            name: "Tamas",
+            text: "Dude, Lilu! Something went wrong at the king's feast!",
+          },
+          { name: "Lilu", text: "Tamas? What's going on?" },
+          {
+            name: "Tamas",
+            text:
+              "Something about an invader in the castle, dude. Pretty spooky!",
+          },
+          {
+            name: "Lilu",
+            text:
+              "An invader...! Oh no, this is Argon's first day as a knight! Thanks Tamas, I gotta go! Oh no...! What's going on here!? Argon, where are you!?",
+          },
+          { name: "Reinhold", text: "Aaargh..." },
+          { name: "Lilu", text: "KING REINHOLD!? Are you alright!?" },
+          { name: "Reinhold", text: "Aahh... My precious life... fleeting..." },
+          {
+            name: "Lilu",
+            text:
+              "This is bad...! Focus Lilu! Remember the Sage training! King Reinhold! Hold still! Heal!",
+          },
+          {
+            name: "Reinhold",
+            text: "It's no use... Please... preserve my beautiful corpse...",
+          },
+          {
+            name: "Lilu",
+            text:
+              "No effect... Oh no... could it be... Poison? ...Just like my parents...! I won't fail this time! I can't! Greater Poison Purge!",
+          },
+          { name: "Reinhold", text: "...Aaaah... Huh?" },
+          {
+            name: "Lilu",
+            text:
+              "King Reinhold! How do you feel? Wait a minute... That cup shielded you from that shuriken! You weren't harmed at all!",
+          },
+          {
+            name: "Reinhold",
+            text:
+              "Oh! Is that so? I suppose I may have overreacted a little bit. A monarch's death is a dramatic affair, after all.",
+          },
+          { name: "Lilu", text: "Argh! Reinhold!" },
+        ],
       },
       {
         rank: 5,
@@ -666,16 +815,102 @@ module.exports = [
               "10% faster quest time. +5% Critical Chance and Evasion for the party.",
           },
         },
+        title: "The Azuman Wanderer",
+        story: [
+          {
+            name: "Troublin Chief",
+            text: "Hee hee hee! What do we have here?",
+          },
+          { name: "Yami", text: "Hmph. Begone, pest." },
+          {
+            name: "Troublin Chief",
+            text: "Y'got some bad manners, lady! And weird clothes!",
+          },
+          { name: "Yami", text: "..." },
+          {
+            name: "Troublin Chief",
+            text:
+              "Ah hah! You smell like Azuma, the eastern human kingdom! What're ya doing all the way over here, lady? Armed to the teeth and tight lipped... Ya gotta be an assassin, right?",
+          },
+          { name: "Yami", text: "..." },
+          {
+            name: "Troublin Chief",
+            text:
+              "Hee hee! I knew it! Stupid humans, always killing each others!",
+          },
+          {
+            name: "Yami",
+            text: "...You're right. I traveled here to kill a tyrannical king.",
+          },
+          {
+            name: "Troublin Chief",
+            text:
+              "Well! Ya can't be a good assassin if you just tell everybody that! Why'd you tell me?",
+          },
+          {
+            name: "Yami",
+            text: "Because you won't live to tell anybody. Farewell.",
+          },
+          { name: "Troublin Chief", text: "Eek!" },
+          { name: "Yami", text: "...King Reinhold, you're next." },
+        ],
       },
       {
         rank: 2,
         coins: 20,
         reward: { health: 2, attack: 45, defense: 20, skill: "---" },
+        title: "Hunt or be Hunted",
+        story: [
+          {
+            name: "Yami",
+            text:
+              "...The castle's security is tight. A password system that changes daily... Unscheduled guard patrols... I'll need to be-",
+          },
+          { name: "Argon", text: "Hi there! You, in the bushes!" },
+          { name: "Yami", text: "...!" },
+          {
+            name: "Argon",
+            text:
+              "Are you here to become a knight too? Sia will be here soon! There's no need to hide, she's pretty nice!",
+          },
+          { name: "Yami", text: "Sia...! I need to leave. Farewell." },
+          { name: "Argon", text: "W-wait! Woah! Smoke bomb!?" },
+          {
+            name: "Yami",
+            text:
+              "...I lost that fool. With Sia here... my task will be difficult. But it must be done. I cannot fail. For the Order... and for my sister's sake.",
+          },
+        ],
       },
       {
         rank: 3,
         coins: 30,
         reward: { health: 7, attack: 136, defense: 62, skill: "---" },
+        title: "Turning Poin",
+        story: [
+          {
+            name: "Reinhold",
+            text:
+              "Esteemed people of my kingdom! Thank you for coming to my sumptuous feast! It is also a perfect opportunity for you all to gaze upon my beautiful self! Do not miss this chance!",
+          },
+          { name: "Yami", text: "(Pompous tyrant. Typical.)" },
+          {
+            name: "Sia",
+            text:
+              "Erm, yes. Thank you for this edifying speech, your Majesty. We have gathered here today to celebrate the King's incredible generosity. In the past months, he has donated several million gold from his own coffers... ...to several local temples, schools and orphanages. All are welcome to partake, so please tell your friends and family. Long live King Reinhold!",
+          },
+          {
+            name: "Yami",
+            text:
+              "(...These are not the actions of a tyrant. ...No, the Order wouldn't lie. This must be a trick.) Tyrant Reinhold, your reign ends now!",
+          },
+          { name: "Sia", text: "A shuriken...! The Azuman assassin!" },
+          { name: "Yami", text: "I missed...!? Impossible!" },
+          {
+            name: "Sia",
+            text: "Guards! Block off all exits! Capture this woman at once!",
+          },
+        ],
       },
       {
         rank: 4,
@@ -690,6 +925,35 @@ module.exports = [
               "20% faster quest time. +10% Critical Chance and Evasion for the party.",
           },
         },
+        title: "Reflection",
+        story: [
+          {
+            name: "Yami",
+            text:
+              "(I hesitated... And failed. Could the Order have lied...? It matters not. I am on my own, now.)",
+          },
+          { name: "Argon", text: "I finally found you!" },
+          { name: "Yami", text: "The foolish knight!" },
+          { name: "Argon", text: "Woah! I'm not here to fight!" },
+          { name: "Yami", text: "I won't fall for your tricks, Reinhold dog!" },
+          { name: "Argon", text: "...You missed on purpose didn't you?" },
+          { name: "Yami", text: "...!" },
+          {
+            name: "Argon",
+            text:
+              "I saw you at the feast. It even looked like you regretted throwing that shuriken. The King's not a bad person, you know? He's a bit weird sometimes, but he tries his best! I'm not sure who paid you to do this, but they can't be good guys... How about going back to the castle and apologizing? I'm sure Reinhold will-",
+          },
+          {
+            name: "Yami",
+            text:
+              "...Apologize? How dense. I have a lot of thinking to do. Don't follow me, if you value your life.",
+          },
+          {
+            name: "Argon",
+            text:
+              "...She's not a bad person, I'm sure of it. Now I need to report to Sia... Oh man, I'm going to get chewed out...",
+          },
+        ],
       },
       {
         rank: 5,
