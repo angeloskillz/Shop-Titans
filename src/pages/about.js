d574cbf3fc6c9c086590ac46263ad345b43d03a1
Skillz <gankstarsleague@gmail.com>
2019-09-16 18:06:36 -0400

    better seo for about

--- src/pages/about.js
+++ src/pages/about.js
@@ -32,7 +32,10 @@ const Hero = styled.div`
 
 export default props => (
   <Layout>
-    <SEO title="About" />
+    <SEO
+      title="About"
+      description="Shop Titans Wiki is the website dedicated to Shop Titans Design & Craft game. Get all the game stats for all blueprints, workers, champions and much more. We are adding guides, news, guild features and much more over time."
+    />
     <Hero>
       <Title>About Us</Title>
       <Description>
